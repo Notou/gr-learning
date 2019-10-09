8d7b373f0f3f995a87df81b986485e15711b81d8
Notou <barthy42@laposte.net>
2019-10-09 16:06:44 +0200

    Embedded python module to align streams based on tags

--- examples/epy_block_0.py
+++ examples/epy_block_0.py
@@ -10,98 +10,118 @@ import numpy as np
 from gnuradio import gr
 import pmt
 
-class blk(gr.basic_block):  # other base classes are basic_block, decim_block, interp_block
-    """Open stream on reception of corr_est tags from the correlation estimator block"""
+class alignement(gr.basic_block):  # other base classes are basic_block, decim_block, interp_block
+    """Align two stream according to a packet number set in a tag"""
 
-    def __init__(self, payload_length=0, preamble_length=80, lookup_window = 30):  # only default arguments here
-        """arguments to this function show up as parameters in GRC"""
-        gr.basic_block.__init__(
-            self,
-            name='Isolator',   # will show up in GRC
-            in_sig=[np.complex64],
-            out_sig=[np.complex64]
-        )
-        # if an attribute with the same name as a parameter is found,
-        # a callback is registered (properties work, too).
-        self.payload_length = payload_length
-        self.preamble_length = preamble_length
-        self.lookup_window = lookup_window
+    def __init__(self, tag_name="packet_num", frame_size=384):
+
+        gr.basic_block.__init__(self,
+            name="Align",
+            in_sig=[np.complex64, np.int8],
+            out_sig=[np.complex64, np.int8])
+        self.frame_size = frame_size
+        self.tag_name = tag_name
+        self.previous_packet = -1
+        self.last_packet_loss = -1
         self.set_tag_propagation_policy(0)
-        self.set_history(max(self.preamble_length, self.lookup_window)+self.payload_length)
-        # print [method for method in dir(gr.block) if callable(getattr(gr.block, method))]
-        # import inspect
-        # print inspect.getmro(blk)
-        # print issubclass(blk, gr.basic_block)
-        self.set_output_multiple((self.preamble_length + self.payload_length) *2)
+        self.set_output_multiple(self.frame_size)
 
     def forecast(self, noutput_items, ninput_items_required):
         # setup size of input_items[i] for work call
         for i in range(len(ninput_items_required)):
-                ninput_items_required[i] = int(noutput_items/2.0)+1
+                ninput_items_required[i] = self.frame_size
+
 
     def general_work(self, input_items, output_items):
-        """example: multiply with constant"""
-        read = self.nitems_read(0)
-        history_read = max(read - self.history()+1,0)
-        max_offset = history_read + len(input_items[0])
-        #print("read",read, max_offset, "length", len(input_items[0]))
-        tags = self.get_tags_in_range(0, history_read , max_offset, pmt.intern("step"))
-        #for tag in tags:
-            #print(tag.key, tag.value, tag.offset)
+        if  len(input_items[0]) < self.frame_size: #Just in case
+            print("Err input size: ", len(input_items[0]), " ", len(input_items[1]))
+            return 0
 
+        read0 = self.nitems_read(0)
+        read1 = self.nitems_read(1)
+        in0 = input_items[0]
+        in1 = input_items[1]
+        out0 = output_items[0]
+        out1 = output_items[1]
+        tags_0 = self.get_tags_in_window(0, 0, len(in0), pmt.intern(self.tag_name))
+        tags_1 = self.get_tags_in_window(1, 0, len(in1), pmt.intern(self.tag_name))
+        value0 = 0
+        value1 = -1
+        offset0 = 0
+        offset1 = 0
 
-        tags = self.get_tags_in_range(0,  history_read + self.preamble_length , max_offset, pmt.intern("corr_est"))
-        tags = list(tags)
-        #print("Input size: ", len(input_items[0]), "   ", len(tags), " tags: ", "type: ", type(tags))
-        #if len(tags)>0 :
-        written = self.nitems_written(0)
-        max_tag_offset = max_offset - self.payload_length - self.lookup_window
-        i=0
-        while i < len(tags):
-            if tags[i].offset > max_tag_offset:
-                tags.remove(tags[i])
-                continue
-            next_index=i+1
-            while next_index < len(tags):
-                #print(tags[next_index].key, tags[next_index].value, tags[next_index].offset)
-                if tags[next_index].offset > tags[i].offset + self.lookup_window:
+        # print("function called: in0: {} in1: {}".format(len(in0), len(in1)))
+        if len(tags_0) == 0:
+            print("We don't have any tag in stream 0")
+            self.consume(0, len(in0))
+            return 0
+        if len(tags_1) == 0:
+            print("We don't have any tag in stream 1")
+            self.consume(1, len(in1))
+            return 0
+
+        #Find the tag in each input that has the same value as the other and is the lowest possible. We assume that the values are increasing
+        for tag0 in tags_0:
+            value0 = pmt.to_python(tag0.value)
+
+            for tag1 in tags_1:
+                value1 = pmt.to_python(tag1.value)
+                offset1 = tag1.offset
+
+                if value0 <= value1:
                     break
-                if pmt.to_python(tags[i].value) > pmt.to_python(tags[next_index].value):
-                    #print("removing next: curr=", tags[i].value, ">  next=", tags[next_index].value, " types: ", type(tags[i].value), type(tags[next_index].value))
-                    tags.remove(tags[next_index])
-                else:
-                    #print("removing current: curr=", tags[i].value, "<  next=", tags[next_index].value)
-                    tags.remove(tags[i])
-            #print(tags[i].key, tags[i].value, tags[i].offset)
-            i += 1
-        output_number = 0
-        pack_length = self.preamble_length+self.payload_length
-        #output_items[0].resize((pack_length*len(tags),))
-        #print(type(output_items[0]), output_items[0].dtype, output_items[0].shape)
-        #print(len(tags))
-        for i in range(len(tags)):
-            #print(tags[i].key, tags[i].value, tags[i].offset)
-            position = tags[i].offset - history_read
-            # if position < self.history():
-            #     print("Start copy at ", position)
-            output_number += pack_length
-            if output_number > len(output_items[0]):
-                print("Too much input was given")
-                self.consume(0, position)
-                return output_number
+                if value0 > value1:
+                    continue
+            if value0 == value1:
+                offset0 = tag0.offset
+                break
+            if value0 < value1:
+                continue
 
-            #print("output number: ", output_number)
-            output_items[0][i*pack_length:(i+1)*pack_length] = input_items[0][position-self.preamble_length:position+self.payload_length]
+        if value0 != value1:
+            #print("Cannot make correspondance between the two streams.", value0, value1)
+            if value0 > value1:
+                self.consume(1, len(in1))
+            if value0 < value1:
+                self.consume(0, len(in0))
+            return 0
 
-            tag_offset = written + i*pack_length + (tags[i].offset - history_read - (position-self.preamble_length))
-            self.add_item_tag(0, tag_offset+20, pmt.intern("header_start"), pmt.to_pmt(0))
-            tags_to_propagate = self.get_tags_in_range(0, tags[i].offset-self.preamble_length, tags[i].offset+self.payload_length)
-            for tag in tags_to_propagate:
-                #if pmt.to_python(tag.key) ==  "step":
-                    #print("Seen: ", tag.key, tag.value, tag.offset)
-                tag_offset = written + i*pack_length + (tag.offset - history_read - (position-self.preamble_length))
-                self.add_item_tag(0, tag_offset, tag.key, tag.value, tag.srcid)
-        self.consume(0, len(input_items[0])-(self.history()-1))
-        if read < self.history():
-            self.consume(0, self.history()-1)
-        return output_number
+        #Index of the selected tags relative to the input arays
+        position0 = offset0 - read0
+        position1 = offset1 - read1
+
+        #We process a frame only if it has been fully received
+        if position0 + self.frame_size >= len(in0):
+            #print("Stream 0 too late, consuming")
+            self.consume(0, position0)
+            return position0
+        if position1 + self.frame_size >= len(in1):
+            #print("Stream 1 too late, consuming")
+            self.consume(1, position1)
+            return position1
+
+        #Detect packet packet loss
+        if value0 != self.previous_packet+1:
+            # for i in range(self.previous_packet+1, value0):
+            print("Packet lost: {} in a row. Value is: {} Packets since last loss: {}".format(value0-(self.previous_packet+1), value0, value0-self.last_packet_loss))
+            self.last_packet_loss = value0
+                # self.message_port_pub(pmt.intern("error"), pmt.to_pmt(self.packet_loss_penalty))
+        if len(out0) < self.frame_size:
+            print("output size 0 not big enough")
+        if len(out1) < self.frame_size:
+            print("output size 1 not big enough")
+        out0[:self.frame_size] = in0[position0:position0 + self.frame_size]
+        out1[:self.frame_size] = in1[position1:position1 + self.frame_size]
+        self.add_item_tag(0, self.nitems_written(0), pmt.intern(self.tag_name), pmt.to_pmt(value0))
+        # print("outputting packet num: {}".format(value0))
+        # diff = in0[position0:position0 + self.frame_size] - in1[position1:position1 + self.frame_size]
+        # norm = numpy.linalg.norm(diff, ord = 2)
+
+        # self.message_port_pub(pmt.intern("error"), pmt.to_pmt(norm.item()))
+        # print(norm, value0)
+
+        self.previous_packet = value0
+        self.consume(0, position0 + self.frame_size)
+        self.consume(1, position1 + self.frame_size)
+
+        return self.frame_size
